db8e2b31942c27a6776bd390ec23bcee89a0d240
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-02-08 22:02:19 +1300

    Little work on input handler click handling

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -2,5 +2,11 @@
 
 std::tuple<int, int> InputHandler::row_col_from_click_coordinates(int number_rows, int number_cols, std::tuple<int, int>& window_dimensions, std::tuple<int, int>& click_position)
 {
-	return std::tuple<int, int>();
+	int sprite_w = std::get<0>(window_dimensions) / number_cols;
+	int sprite_h = std::get<1>(window_dimensions) / number_rows;
+
+	int row = std::get<0>(click_position) / sprite_w;
+	int col = std::get<1>(click_position) / sprite_h;
+
+	return std::tuple<int, int>(row, col);
 }
