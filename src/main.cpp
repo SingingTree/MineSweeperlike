a80d6a187b28f65e2a62cb5ab573a80221d23784
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-02-23 21:50:39 +1300

    Break main up into more functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,74 @@
 #include "Renderer.h"
 #include "InputHandler.h"
 
-int main(int argc, char** argv)
+// Returns a bool indicating if the game should continue (true) or terminate (false)
+bool game_handle_input(SDL_Event &event, Renderer &renderer, Board &board, InputHandler &inputHandler)
 {
-	Board board(10, 10, 2);
-	Renderer renderer;
-	InputHandler inputHandler;
-	if(renderer.init() != 0)
+	// Close window
+	if(event.type == SDL_QUIT)
 	{
-		renderer.quit();
-		return 1;
+		return false;
 	}
+	// Presses any key
+	if(event.type == SDL_KEYDOWN)
+	{
+		return false;
+	}
+	// Click the mouse
+	if(event.type == SDL_MOUSEBUTTONDOWN)
+	{
+		std::tuple<int, int> click_pos(event.button.x, event.button.y);
+		std::tuple<int, int> row_and_col = inputHandler.row_col_from_click_coordinates(
+			board.get_height(),
+			board.get_width(),
+			renderer.get_window_dimensions(),
+			click_pos
+			);
+		board.select_tile(std::get<0>(row_and_col), std::get<1>(row_and_col));
+		std::cout << std::get<0>(row_and_col) << ", " << std::get<1>(row_and_col) << std::endl;
+	}
+	return true;
+}
 
+void game_loop(Renderer &renderer, Board &board)
+{
+	InputHandler inputHandler;
 	bool running = true;
 	SDL_Event event;
 	while(running)
 	{
 		while(SDL_PollEvent(&event))
 		{
-			// Close window
-			if(event.type == SDL_QUIT)
-			{
-				running = false;
-			}
-			// Presses any key
-			if(event.type == SDL_KEYDOWN)
-			{
-				running = false;
-			}
-			// Click the mouse
-			if(event.type == SDL_MOUSEBUTTONDOWN)
-			{
-				std::tuple<int, int> click_pos(event.button.x, event.button.y);
-				std::tuple<int, int> row_and_col = inputHandler.row_col_from_click_coordinates(
-					board.get_height(),
-					board.get_width(),
-					renderer.get_window_dimensions(),
-					click_pos
-					);
-				board.select_tile(std::get<0>(row_and_col), std::get<1>(row_and_col));
-				std::cout << std::get<0>(row_and_col) << ", " << std::get<1>(row_and_col) << std::endl;
-			}
-
+			running = game_handle_input(event, renderer, board, inputHandler);
 		}
 		std::cout << board.get_game_state() << std::endl;
 		renderer.render(board);
+	}
+}
 
+int game()
+{
+	// Setup board
+	Board board(10, 10, 2);
+	// Setup renderer
+	Renderer renderer;
+	// Handle renderer setup
+	if(renderer.init() != 0)
+	{
+		renderer.quit();
+		return 1;
 	}
 
+	// If we've made it this far we can start the game!
+	game_loop(renderer, board);
+
+	// Once game's done destory our renderer
 	renderer.quit();
 
 	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	return game();
 }
